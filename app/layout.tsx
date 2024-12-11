46b736f0f43ef4fc27afe127f17bf48e7f4f163b
Tejas <62064506+KidCoderT@users.noreply.github.com>
2024-12-11 12:28:01 +0530

    Added Init Dialog + Some more details

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,14 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 export const metadata: Metadata = {
   title: "ProThink",
@@ -15,8 +23,49 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="antialiased">
-        <main>{children}</main>
-        <Toaster />
+        <Dialog defaultOpen>
+          <main>{children}</main>
+
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Welcome to ProThink</DialogTitle>
+            </DialogHeader>
+            <div className="text-pretty">
+              <p className="text-lg mb-4 leading-relaxed">
+                I designed this app as a simple project with a powerful purpose
+                - to help you become a better problem solver. While it functions
+                as an online code editor, its primary focus is on the journey of
+                problem-solving. Instead of directly providing code solutions,
+                it offers helpful hints and suggestions, explaining any problems
+                and difficulties you may encounter. The AI implements the
+                Socratic method, engaging you in thoughtful dialogue to reach
+                solutions.
+              </p>
+
+              <p className="text-lg mb-4 leading-relaxed">
+                You'll find the code editor in the top right corner where you
+                can write your code. Just above it is a run button that will
+                open the testcases tab. Of course, you can also manually access
+                the testcases by clicking the tab directly.
+              </p>
+
+              <p className="text-lg mb-4 leading-relaxed">
+                In the bottom right, you'll see the chat tab - your portal for
+                interacting with the AI. Feel free to ask questions and receive
+                guided assistance.
+              </p>
+
+              <p className="text-lg mb-4 leading-relaxed">
+                The problem to solve appears on the left side. While there's
+                currently only one challenge available (sourced from the Advent
+                of Code 2023), I hope you'll enjoy the solving process
+                nonetheless!
+              </p>
+            </div>
+          </DialogContent>
+
+          <Toaster />
+        </Dialog>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Chat from "@/components/Chat";
 import { CodeEditor } from "@/components/CodeEditor.1";
 import Question from "@/components/Question";
+import { Button } from "@/components/ui/button";
+import { DialogTrigger } from "@/components/ui/dialog";
 
 import {
   ResizableHandle,
@@ -8,13 +10,19 @@ import {
   ResizablePanelGroup,
 } from "@/components/ui/resizable";
 import { questions } from "@/data/question";
+import { QuestionMarkIcon } from "@radix-ui/react-icons";
 
 export default function Home() {
   return (
     <div className="w-full h-screen">
       <ResizablePanelGroup direction="horizontal" className="w-full">
         <ResizablePanel defaultSize={50}>
-          <div className="flex h-screen justify-center w-full flex-col ">
+          <div className="flex h-screen justify-center w-full flex-col relative">
+            <DialogTrigger asChild>
+              <Button className="absolute right-10 top-10">
+                Help <QuestionMarkIcon className="size-4" />{" "}
+              </Button>
+            </DialogTrigger>
             <Question markdown={questions[0].markdown} />
           </div>
         </ResizablePanel>
